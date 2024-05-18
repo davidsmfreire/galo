5d691bf7acab12cd08e6b4722fd134ce6d2878e5
davidsmfreire <davidsmfreire@gmail.com>
2024-05-18 23:59:22 +0100

    feat: optimize dockerfiles and have entrypoint in dockerfile

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ echo "Building docker image for $1..." && \
 docker build . -q --tag galo_$1
 
 docker container rm -f galo_$1 1>/dev/null 2>/dev/null || true && \
-docker run --name galo_$1 -it galo_$1 "/main"
+docker run --name galo_$1 -it galo_$1
